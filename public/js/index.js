fae57d311fed013363260eccfd8c908ab3ea59d0
unknown <miha.drofenik92@gmail.com>
2018-10-28 20:52:43 +0100

    Added some button tweaks

--- public/js/index.js
+++ public/js/index.js
@@ -31,11 +31,13 @@ socket.emit("createMessage", {
 $("#message-form").on("submit", function(e) {
     e.preventDefault()
 
+    var messageTextbox =  $("[name=message]")
+
     socket.emit("createMessage", {
         from:"user",
-        text: $("[name=message]").val()
+        text: messageTextbox.val()
     }, function () {
-
+        messageTextbox.val("")
     })
 })
 
@@ -44,12 +46,16 @@ locationButton.on("click", function () {
     if (!navigator.geolocation) {
         return alert("Geolocation not supported by your browser.")
     }
+    $(locationButton.attr("disabled", true)).text("Sending location...")
     navigator.geolocation.getCurrentPosition(function (position) {
+        $(locationButton.removeAttr("disabled")).text("Send location")
         socket.emit("createLocationMessage", {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         })
+        $locationButton.attr("disabled", true)
     }, function () {
+        $(locationButton.removeAttr("disabled")).text("Send location")
         alert("Unable to fetch location")
     })
 })
