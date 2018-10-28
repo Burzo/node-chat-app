13ad1e661eec0094aaa59842198f9b2a7244e408
unknown <miha.drofenik92@gmail.com>
2018-10-28 11:30:49 +0100

    socket.io functionality

--- server/server.js
+++ server/server.js
@@ -1,15 +1,38 @@
 const path = require("path")
+const http = require("http")
 const express = require("express")
 const app = express()
+const socketIO = require("socket.io")
 
 const publicPath = path.join(__dirname, "../public")
 const port = process.env.PORT || 3000
 
-app.use("/static", express.static(publicPath))
+const server = http.createServer(app)
+var io = socketIO(server)
+
+app.use(express.static(publicPath))
+
+io.on("connection", (socket) => {
+    console.log("New user connected")
+
+    socket.on("createMessage", (message) => {
+        console.log("createMessage", message)
+        io.emit("newMessage", {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        })
+    })
+
+    socket.on("disconnect", () => {
+        console.log("User was disconnected.")
+    })
+})
+
 
 app.get("/", ((req, res) => {
     res.sendFile(publicPath+"/index.html")
 }))
 
-app.listen(port, ()=>{console.log(`--- SERVER IS UP ON PORT ${port} ---`)})
+server.listen(port, () => {console.log(`--- SERVER IS UP ON PORT ${port} ---`)})
 
